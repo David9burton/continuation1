e0a59f070c5f9a8de8cf0715b3db6250c86dcec7
Burton <burteendavi@gmail.com>
2023-08-26 21:26:54 +0300

    Corrected

--- improvedcontactwithdatetime.py
+++ improvedcontactwithdatetime.py
@@ -1,49 +1,79 @@
-import datetime
+import os
+from datetime import datetime
 
 def load_contacts():
-    # ... [No changes here]
+    if os.path.exists("contacts.txt"):
+        with open("contacts.txt", "r") as file:
+            lines = file.readlines()
+            return {line.split(',')[0]: {"phone": line.split(',')[1], "email": line.split(',')[2], "date_added": line.split(',')[3].strip()} for line in lines}
+    return {}
 
 def save_contacts(contacts):
-    # ... [No changes here]
+    with open("contacts.txt", "w") as file:
+        for name, details in contacts.items():
+            file.write(f"{name},{details['phone']},{details['email']},{details['date_added']}\n")
 
-def add_contact(contacts):
-    # ... [No changes here]
-
-def view_contacts(contacts):
-    # Sort contacts by name before displaying
-    sorted_contacts = dict(sorted(contacts.items()))
-    for name, details in sorted_contacts.items():
-        print(f"Name: {name}, Phone: {details['phone']}, Email: {details['email']}, Date Added: {details['date']}, Notes: {details['notes']}")
+def backup_contacts(contacts):
+    with open("contacts_backup.txt", "w") as file:
+        for name, details in contacts.items():
+            file.write(f"{name},{details['phone']},{details['email']},{details['date_added']}\n")
+    print("Contacts backed up successfully!")
 
 def search_contact(contacts):
-    name = input("Enter the name of the contact to search: ")
-    if name in contacts:
-        details = contacts[name]
-        print(f"Name: {name}, Phone: {details['phone']}, Email: {details['email']}, Date Added: {details['date']}, Notes: {details['notes']}")
+    search_name = input("Enter the name of the contact to search: ")
+    if search_name in contacts:
+        details = contacts[search_name]
+        print(f"Name: {search_name}, Phone: {details['phone']}, Email: {details['email']}, Date Added: {details['date_added']}")
     else:
         print("Contact not found!")
 
+def add_contact(contacts):
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    email = input("Enter email address: ")
+    date_added = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    contacts[name] = {"phone": phone, "email": email, "date_added": date_added}
+    save_contacts(contacts)
+
+def view_contacts(contacts):
+    for name, details in contacts.items():
+        print(f"Name: {name}, Phone: {details['phone']}, Email: {details['email']}, Date Added: {details['date_added']}")
+
 def update_contact(contacts):
-    # ... [No changes here]
+    name = input("Enter the name of the contact to update: ")
+    if name in contacts:
+        phone = input("Enter new phone number: ")
+        email = input("Enter new email address: ")
+        contacts[name] = {"phone": phone, "email": email, "date_added": contacts[name]['date_added']}
+        save_contacts(contacts)
+    else:
+        print("Contact not found!")
 
 def delete_contact(contacts):
-    # ... [No changes here]
+    name = input("Enter the name of the contact to delete: ")
+    if name in contacts:
+        del contacts[name]
+        save_contacts(contacts)
+    else:
+        print("Contact not found!")
 
 def main():
     contacts = load_contacts()
     while True:
-        choice = input("\n1. Add Contact\n2. View Contacts\n3. Search Contact\n4. Update Contact\n5. Delete Contact\n6. Exit\nEnter your choice: ")
+        choice = input("\n1. Add Contact\n2. View Contacts\n3. Update Contact\n4. Delete Contact\n5. Search Contact\n6. Backup Contacts\n7. Exit\nEnter your choice: ")
         if choice == "1":
             add_contact(contacts)
         elif choice == "2":
             view_contacts(contacts)
         elif choice == "3":
-            search_contact(contacts)
-        elif choice == "4":
             update_contact(contacts)
-        elif choice == "5":
+        elif choice == "4":
             delete_contact(contacts)
+        elif choice == "5":
+            search_contact(contacts)
         elif choice == "6":
+            backup_contacts(contacts)
+        elif choice == "7":
             break
         else:
             print("Invalid choice!")
